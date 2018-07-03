edfa2b1b4fefa0df471bc1679f0ff9bc62686863
sfus <sfus.dev@gmail.com>
2018-07-03 21:33:56 +0900

    Add tmux formula

---
+++ tmux.rb
@@ -0,0 +1,48 @@
+class Tmux < Formula
+  desc "Terminal multiplexer"
+  homepage "https://tmux.github.io/"
+  head "https://github.com/sfus/tmux", using: :git, branch: '2.7-border-ascii-ex'
+  version '2.7'
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on "utf8proc" => :optional
+
+  resource "completion" do
+    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
+    sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
+  end
+
+  def install
+    system "sh", "autogen.sh" if build.head?
+
+    args = %W[
+      --disable-Dependency-tracking
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+    ]
+
+    args << "--enable-utf8proc"
+
+    ENV.append "LDFLAGS", "-lresolv"
+    system "./configure", *args
+
+    system "make", "install"
+
+    pkgshare.install "example_tmux.conf"
+    bash_completion.install resource("completion")
+  end
+
+  def caveats; <<~EOS
+    Example configuration has been installed to:
+      #{opt_pkgshare}
+    EOS
+  end
+
+  test do
+    system "#{bin}/tmux", "-V"
+  end
+end
